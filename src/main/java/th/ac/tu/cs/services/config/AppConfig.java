2e89e7757cbeab33e5a556e690dfbccc7159041a
rapeephong-6509681216 <rapeephong.man@dome.tu.ac.th>
2023-11-13 18:15:18 +0700

    Added login authentication using TU API

---
+++ src/main/java/th/ac/tu/cs/services/config/AppConfig.java
@@ -0,0 +1,14 @@
+package th.ac.tu.cs.services.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
