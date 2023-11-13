4c8f534c073c17474e3d041f01841312855259ad
rapeephong-6509681216 <rapeephong.man@dome.tu.ac.th>
2023-11-13 18:53:29 +0700

    Added login authentication using TU API

---
+++ src/main/java/th/ac/tu/cs/services/controllers/LoginController.java
@@ -0,0 +1,51 @@
+package th.ac.tu.cs.services.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import th.ac.tu.cs.services.models.User;
+import th.ac.tu.cs.services.services.TuApiService;
+
+@Controller
+public class LoginController {
+
+    private final TuApiService tuApiService;
+
+    public LoginController(TuApiService tuApiService) {
+        this.tuApiService = tuApiService;
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    /**
+     * Handles the POST request for the login route.
+     *
+     * @param username The username of the user trying to log in.
+     * @param password The password of the user trying to log in.
+     * @param model The model object to bind data to the view.
+     * @return The name of the view to be rendered. If authentication is successful, it returns "data", otherwise "login".
+     */
+    @PostMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password, Model model) {
+        User user = tuApiService.authenticate(username, password);
+        if (user != null) {
+            model.addAttribute("user", user);
+            return "data";
+        } else {
+            model.addAttribute("error", "Invalid username or password. Please try again.");
+            return "login";
+        }
+    }
+
+    @GetMapping("/data")
+    public String data() {
+        return "data";
+    }
+
+}
