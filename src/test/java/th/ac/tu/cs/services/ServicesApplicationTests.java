037f402efec4c0bfb1347fa5073265dd9f664c9a
rapeephong-6509681216 <rapeephong.man@dome.tu.ac.th>
2023-11-15 19:01:28 +0700

    reset

---
+++ src/test/java/th/ac/tu/cs/services/ServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package th.ac.tu.cs.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
