ac906785b0b49c8fb97d1a72457aaf92d4415b83
teeraphat <teeraphat.sir@dome.tu.ac.th>
2023-11-16 20:27:45 +0700

    debugging controller

--- src/main/java/th/ac/tu/cs/services/controller/FormInfoController.java
+++ src/main/java/th/ac/tu/cs/services/controller/FormInfoController.java
@@ -5,6 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import th.ac.tu.cs.services.model.FormInfo;
 import th.ac.tu.cs.services.repository.JdbcFormInfoRepository;
+@RestController
+@RequestMapping("/api/reg")
+@CrossOrigin
 
 @Controller
 public class FormInfoController {
@@ -21,25 +24,31 @@ public class FormInfoController {
     }
 
     @PostMapping("/index")
-    public String form(@RequestParam String JSON1, Model model) {
+    public String form(@RequestBody String JSON1, Model model) {
+        System.out.println(JSON1);
         FormInfo info = new FormInfo();
         System.out.println("check post index");
-        info.setUsername(JSON1);
-        info.setStudentFirstName(JSON1);
-        info.setStudentLastName(JSON1);
-        info.setNameTitles(JSON1);
-        info.setStudentYear(JSON1);
-        info.setStudyField(JSON1);
-        info.setMobilePhone(JSON1);
-        info.setPhone(JSON1);
-        info.setAdvisor(JSON1);
-        info.setAddressNumber(JSON1);
-        info.setMoo(JSON1);
-        info.setTumbol(JSON1);
-        info.setAmphur(JSON1);
-        info.setProvince(JSON1);
-        info.setPostalcode(JSON1);
-        jdbcFormInfoRepository.saveInfo(info);
+        try{
+            info.setUsername(JSON1);
+            info.setStudentFirstName(JSON1);
+            info.setStudentLastName(JSON1);
+            info.setNameTitles(JSON1);
+            info.setStudentYear(JSON1);
+            info.setStudyField(JSON1);
+            info.setMobilePhone(JSON1);
+            info.setPhone(JSON1);
+            info.setAdvisor(JSON1);
+            info.setAddressNumber(JSON1);
+            info.setMoo(JSON1);
+            info.setTumbol(JSON1);
+            info.setAmphur(JSON1);
+            info.setProvince(JSON1);
+            info.setPostalcode(JSON1);
+            jdbcFormInfoRepository.saveInfo(info);
+        }catch (Exception e){
+            System.err.println(e);
+        }
+        System.out.println(info);
         return "index";
     }
 }
